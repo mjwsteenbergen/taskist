5830d9ed3c0ba94bb17950c2f68baf2dbdc7013d
Martijn Steenbergen <m.uses+git@outlook.com>
2023-07-16 13:24:20 +0200

    move taskist to its own folder

--- taskist/src/main.tsx
+++ taskist/src/main.tsx
@@ -5,17 +5,21 @@ import { ProjectContextProvider } from './context/project';
 import { SectionContextProvider } from './context/section';
 import { TodoContextProvider } from './context/task';
 import { Tasks } from './items';
+import { Main } from './srcv2/main';
 
 ReactDOM.createRoot(document.getElementsByTagName('body')[0]).render(
   <React.StrictMode>
-    <div className='grid min-h-screen content-center justify-center my-8'>
-        <TodoContextProvider v={[]}>
-          <ProjectContextProvider v={[]}>
-            <SectionContextProvider v={[]}>
-              <Tasks />
-            </SectionContextProvider>
-          </ProjectContextProvider>
-        </TodoContextProvider>
-      </div>
+        <Main/>
   </React.StrictMode>,
 )
+
+  // < div className = 'grid min-h-screen content-center justify-center py-8' >
+
+  // < TodoContextProvider v = { []} >
+  //   <ProjectContextProvider v={[]}>
+  //     <SectionContextProvider v={[]}>
+  //       <Tasks />
+  //     </SectionContextProvider>
+  //   </ProjectContextProvider>
+  //       </TodoContextProvider >
+    // </div >
